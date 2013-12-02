a25531940d55f3d47f5f978dbe7911c8b6b73091
shamithc <shamithc@gmail.com>
2013-12-02 17:36:05 +0530

    Rmove Generator: testABC

--- lib/generators/abc/abc_generator.rb
+++
@@ -1,8 +0,0 @@
-module Tailf
-	class AbcGenerator < Rails::Generators::Base
-	  # source_root File.expand_path('../templates', __FILE__)
-	  def create_initializer_file
-	    create_file "config/initializers/initializer_1.rb", "# Add initialization content here"
-	  end
-	end
-end
