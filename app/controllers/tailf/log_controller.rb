7c87458cd949bbf298a9f4d044063412067da1b5
Vishnu VN <vishnuvntcr@gmail.com>
2013-12-02 16:26:48 +0530

    Yeeee

--- app/controllers/tailf/log_controller.rb
+++ app/controllers/tailf/log_controller.rb
@@ -1,12 +1,22 @@
 require_dependency "tailf/application_controller"
 
 module Tailf
-  class LogController < ApplicationController
-    def index
-    	# Logging.logger.root.level = 'off'
-    	@last_25_lines = `tail -n 250 "#{Tailf.rails_log_path}"`
-    	#Rails.application.config['log'] = 'aaa'
-    	render text: @last_25_lines
+	class LogController < ApplicationController
+		def index
+    	file_exist = File.exist?("#{Tailf.rails_log_path}")
+    	if file_exist
+    		counter = 1
+    		@file_content = []
+    			file = File.new("#{Tailf.rails_log_path}", "r")
+    			while (line = file.gets)
+    				@file_content <<  line
+    				counter = counter + 1
+    			end
+    			file.close
+    	else
+    		@file = "Sorry file not exist!"
+    	end
+        @file_content = @file_content.reverse
     end
-  end
+end
 end
